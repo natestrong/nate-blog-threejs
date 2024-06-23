fcbc0bda2fee930cb872f5f9fdd7e54e13fc85be
Nathan Strong <nathanstrong.fx@gmail.com>
2024-06-23 14:52:22 -0700

    lets gooo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {ThreeSceneComponent} from './three-scene/three-scene.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ThreeSceneComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'nate-blog-threejs';
